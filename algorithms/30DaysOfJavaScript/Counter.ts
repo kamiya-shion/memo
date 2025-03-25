fffe491b89efacc284d38d6138f4e88a0b67d0c4
kamiya <nanahumi0928@gmail.com>
2025-03-26 08:38:37 +0900

    Counter

---
+++ algorithms/30DaysOfJavaScript/Counter.ts
@@ -0,0 +1,13 @@
+// NOTE: - 後置インクリメント
+function createCounter(n: number): () => number {
+  return function () {
+    return n++;
+  };
+}
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
