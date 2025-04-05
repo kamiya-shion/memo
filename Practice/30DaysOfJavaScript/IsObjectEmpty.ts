c3bf0772597b8dcd3fc1a8fbe4a80a9551950f08
kamiya <nanahumi0928@gmail.com>
2025-04-05 11:34:11 +0900

    IsObjectEmpty

---
+++ Practice/30DaysOfJavaScript/IsObjectEmpty.ts
@@ -0,0 +1,13 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+
+type Obj = Record<string, JSONValue> | JSONValue[];
+
+function isEmpty(obj: Obj): boolean {
+  return Array.isArray(obj) ? obj.length === 0 : Object.keys(obj).length === 0;
+}
